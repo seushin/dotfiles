7d6dfed3ed317357932ce130886ac1d7702d65ac
seushin <seushin@student.42seoul.kr>
2021-01-22 11:32:08 +0900

    move vim install script from install.sh to install-vim.sh

---
+++ install-vim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd $(dirname $BASH_SOURCE)
+BASE=$(pwd)
+
+# vim-plug
+mkdir -p ~/.vim/autoload
+[ -e ~/.vim/autoload/plug.vim ] || curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf $BASE/.ycm_extra_conf.py ~/.vim/
+
+# vimrc
+mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
+ln -sf $BASE/vimrc ~/.vimrc
+
+# nvim
+mkdir -p ~/.config/nvim/autoload
+ln -sf $BASE/vimrc ~/.config/nvim/init.vim
+ln -sf ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/
+
+vim +PlugInstall +qall
--- install.sh
+++ install.sh
@@ -26,23 +26,3 @@ echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zs
 
 git config --global user.email "seushin@student.42seoul.kr"
 git config --global user.name "seushin"
-
-# install-vim
-cd $(dirname $BASH_SOURCE)
-BASE=$(pwd)
-
-# vim-plug
-mkdir -p ~/.vim/autoload
-[ -e ~/.vim/autoload/plug.vim ] || curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sf $BASE/.ycm_extra_conf.py ~/.vim/
-
-# vimrc
-mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
-ln -sf $BASE/vimrc ~/.vimrc
-
-# nvim
-mkdir -p ~/.config/nvim/autoload
-ln -sf $BASE/vimrc ~/.config/nvim/init.vim
-ln -sf ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/
-
-vim +PlugInstall +qall
