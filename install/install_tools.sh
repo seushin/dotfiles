c829822ef6b4cf125f3133be50567087c99efd85
seushin <seushin@student.42seoul.kr>
2021-07-14 18:51:11 +0900

    인스톨러 조건문 수정

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -2,19 +2,23 @@
 
 set -x
 
-if [[ ! "$(command -v brew)" ]]; then
-	if [[ "$(uname -s)" = "Darwin" ]]; then
+if [ ! "$(command -v brew)" ]; then
+	if [ "$(uname -s)" = "Darwin" ]; then
 		# if is cluster use this- rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc
 		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	fi
-	else if [[ "$(uname -s)" = "Linux" ]]; then
+	elif [ "$(uname -s)" = "Linux" ]; then
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ./config/zsh/include/tools.zsh
+		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 	fi
 fi
 
 if [ ! -e ~/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
-brew install tmux neovim node yarn llvm bat zsh-syntax-highlighting
-npm i -g git-split-diffs
+if [ "$(command -v brew)" ]; then
+	brew install tmux neovim node yarn llvm bat zsh-syntax-highlighting pure
+fi
+if [ "$(command -v npm)" ]; then
+	npm i -g git-split-diffs
+fi
